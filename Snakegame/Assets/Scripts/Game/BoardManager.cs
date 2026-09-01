6a228c7e6aabbf771d7485aef0020f7609497e2a
DESKTOP-94M8TSN\wing9 <wing997@naver.com>
2026-09-01 18:38:12 +0900

    맵 생성 로직 수정 - 맵 생성 잘못되고 있던 부분 수정 - 바닥 타일 여러개 사용할 수 있도록 수정

--- Snakegame/Assets/Scripts/Game/BoardManager.cs
+++ Snakegame/Assets/Scripts/Game/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -10,19 +11,19 @@ public class BoardManager : MonoBehaviour
     [SerializeField] private int height = DEFAULT_MAP_SIZE;   // 맵 높이
     
     [Header("Tilemap")]
-    [SerializeField] private Tilemap floorTilemap;  // 맵 타일맵
-    [SerializeField] private Tilemap wallTilemap;  // 벽 타일맵
+    [SerializeField] private Tilemap floorTilemap;            // 맵 타일맵
+    [SerializeField] private Tilemap wallTilemap;             // 벽 타일맵
 
     [Header("Tiles")] 
-    [SerializeField] private TileBase floorTile;    // 맵 타일
-    [SerializeField] private TileBase wallTile;     // 벽 타일
+    [SerializeField] private List<TileBase> floorTiles;    // 맵 타일
+    [SerializeField] private TileBase wallTile;             // 벽 타일
     
     public int Witdh => width;
     public int Height => height;
 
     private void Start()
     {
-        GenerateMap();
+        GenerateMap(width, height);
     }
     
     // 타일맵 생성 함수
@@ -40,12 +41,24 @@ public class BoardManager : MonoBehaviour
     // 바닥 생성 함수
     public void GenerateFloor(int xSize, int ySize)
     {
-        for (int y = 0; y < xSize; y++)
+        for (int y = 0; y < ySize; y++)
         {
-            for (int x = 0; x < ySize; x++)
+            for (int x = 0; x < xSize; x++)
             {
                 Vector3Int tilePosition = new Vector3Int(x, y, 0);
-                floorTilemap.SetTile(tilePosition, floorTile);
+
+                // 현재 좌표 기준으로 바둑판 타일 패턴 처리
+                int tileIndex = 0;  // 맵 타일 인덱스
+                if (floorTiles.Count > 0)
+                {
+                    tileIndex = (x + y) % floorTiles.Count;  // 맵 타일 인덱스
+                }
+                else
+                {
+                    Debug.LogError("GenerateFloor:: No floor tiles in the list!");
+                }
+                
+                floorTilemap.SetTile(tilePosition, floorTiles[tileIndex]);
             }
         }
     }
@@ -56,13 +69,13 @@ public class BoardManager : MonoBehaviour
         for (int x = -1; x <= xSize; x++)
         {
             wallTilemap.SetTile(new Vector3Int(x, -1, 0), wallTile);
-            wallTilemap.SetTile(new Vector3Int(x, xSize, 0), wallTile);
+            wallTilemap.SetTile(new Vector3Int(x, ySize, 0), wallTile);
         }
 
         for (int y = -1; y <= ySize; y++)
         {
             wallTilemap.SetTile(new Vector3Int(-1, y, 0), wallTile);
-            wallTilemap.SetTile(new Vector3Int(ySize, y, 0), wallTile);
+            wallTilemap.SetTile(new Vector3Int(xSize, y, 0), wallTile);
         }
     }
 
